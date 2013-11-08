052f4a2e01b91484de0b1683e1ea9c097d01d9d3
Administrator <Administrator@10.103.29.242>
2013-11-08 17:52:18 +0800

    821 windows edited

---
+++ src/Chapter4TreeGraph/Question4_3.java
@@ -0,0 +1,15 @@
+package Chapter4TreeGraph;
+
+import java.util.ArrayList;
+
+public class Question4_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] list = {2, 4, 6, 1, 3, 9, 14, 15, 10, 0, 5, 7, 8, 11, 12, 13};
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.
+	}
+
+}
+
